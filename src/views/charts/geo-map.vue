10480ac2286e8301d5f546613dfd236520a287df
e <1510748736@qq.com>
2020-10-15 19:29:45 +0800

    添加GeoMap

---
+++ src/views/charts/geo-map.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="chart-container">
+    <geo :height="'100%'" :width="'100%'" />
+  </div>
+</template>
+
+<script>
+import Geo from '@/components/Charts/GeoMap'
+
+export default {
+  name: 'GeoMap',
+  components: { Geo }
+}
+</script>
+
+<style scoped>
+.chart-container{
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 84px);
+}
+</style>
